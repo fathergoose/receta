a646a2d2e1ffe0f62fc91c1dc14b6e03ee78e066
fathergoose <al@ilseman.com>
2016-05-19 09:54:58 -0500

    configure asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module Receta
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add out Bower components into rails
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap-sass-official", "assets", "fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
   end
 end
